00e0a172573d97e3bba8e554eafb0e5458f9dc6c
msotnik <frost1kk04@gmail.com>
2025-08-27 19:29:04 +0300

    Доработка логики проекта

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -3,5 +3,6 @@ package com.stepup.proj;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("HelloWorld!");
+        System.out.println("111111");
     }
 }
