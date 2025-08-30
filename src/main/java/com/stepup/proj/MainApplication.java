55f6bdb40849209fdb12596cb2a4b0f46b9a29df
msotnik <frost1kk04@gmail.com>
2025-08-30 10:18:16 +0300

    Изменено doSomething + сообщение

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -9,9 +9,7 @@ public class MainApplication {
     }
 
     public static void doSomething (){
-        System.out.println(0);
-        System.out.println(1);
-        System.out.println(1);
+        System.out.println("Hello");
     }
 
 }
