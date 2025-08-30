309c48b5cc9007ee4cd0ead7119a5068eac81285
msotnik <frost1kk04@gmail.com>
2025-08-30 09:32:05 +0300

    Доработка по sout, доработали логику main и doSomething

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -2,14 +2,15 @@ package com.stepup.proj;
 
 public class MainApplication {
     public static void main(String[] args) {
+        System.out.println("---");
         System.out.println("HelloWorld!");
         System.out.println("1000");
     }
 
-    public static void doSemething (){
+    public static void doSomething (){
+        System.out.println(0);
         System.out.println(1);
-        System.out.println(2);
-        System.out.println(3);
+        System.out.println(1);
     }
 
 }
